590dfa1c1c124269bd6785ece9692a7957ab38a0
dsyncerek <dsyncerek@outlook.com>
2018-10-03 13:08:00 +0200

    Minify images only in production mode

--- gulp-tasks/images.js
+++ gulp-tasks/images.js
@@ -1,11 +1,11 @@
-const gulp = require('gulp');
 const image = require('gulp-image');
+const util = require('gulp-util');
 
 module.exports = function () {
-  const {files} = this.context;
+  const {files, production} = this.context;
   const gulp = this.gulp;
 
   return gulp.src(files.images.source)
-             .pipe(image({quiet: true}))
+             .pipe(production ? image({quiet: true}) : util.noop())
              .pipe(gulp.dest(files.images.destination));
 };
