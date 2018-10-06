378e8a96afeb7d07bb8cf47846fcf38c830168bf
dsyncerek <dsyncerek@outlook.com>
2018-10-06 18:00:14 +0200

    Do not copy empty dirs

--- gulp-tasks/copy.js
+++ gulp-tasks/copy.js
@@ -2,6 +2,6 @@ module.exports = function () {
   const {files} = this.context;
   const gulp = this.gulp;
 
-  return gulp.src(files.copy.source)
+  return gulp.src(files.copy.source, {nodir: true})
              .pipe(gulp.dest(files.copy.destination));
 };
