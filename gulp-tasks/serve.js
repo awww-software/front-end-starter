5c1eaf43b5fa2961391e2ff3c216c90fc1fe2b11
dsyncerek <dsyncerek@outlook.com>
2018-10-24 11:50:04 +0200

    Add `browsersync` logging

--- gulp-tasks/serve.js
+++ gulp-tasks/serve.js
@@ -4,7 +4,6 @@ module.exports = function (done) {
   const {dirs} = this.context;
 
   const browsersyncConfig = {
-    logLevel: 'silent',
     notify: false,
     server: {baseDir: dirs.build},
     files: [`${dirs.build}/**/*`]
